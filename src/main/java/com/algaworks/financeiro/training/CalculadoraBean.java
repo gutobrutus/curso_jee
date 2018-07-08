0acd7fe8a86c7cf16bd3e66b0c2ed8ca90fb361a
gutofunny <gutoinfo.ribeiro@gmail.com>
2018-07-08 19:27:49 -0300

    Demais modificações e treino com validação e conversão 1

---
+++ src/main/java/com/algaworks/financeiro/training/CalculadoraBean.java
@@ -0,0 +1,37 @@
+package com.algaworks.financeiro.training;
+
+public class CalculadoraBean {
+	private Double valorA;
+	private Double valorB;
+	private Double resultado;
+	
+	public void somar() {
+		this.resultado = this.valorA + this.valorB;
+	}
+
+	public Double getValorA() {
+		return valorA;
+	}
+
+	public void setValorA(Double valorA) {
+		this.valorA = valorA;
+	}
+
+	public Double getValorB() {
+		return valorB;
+	}
+
+	public void setValorB(Double valorB) {
+		this.valorB = valorB;
+	}
+
+	public Double getResultado() {
+		return resultado;
+	}
+
+	public void setResultado(Double resultado) {
+		this.resultado = resultado;
+	}
+	
+	
+}
